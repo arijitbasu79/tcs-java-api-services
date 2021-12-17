76573bfd9de2d0a20ee56aba45fc2f6b81a18c6e
Arijit Basu <86120566+arijitbasu79@users.noreply.github.com>
2021-12-18 02:31:36 +0530

    Add files via upload

---
+++ src/main/java/com/tcsl/javaapi/TcsJavaApiServicesApplication.java
@@ -0,0 +1,32 @@
+package com.tcsl.javaapi;
+
+import java.util.Locale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@SpringBootApplication
+public class TcsJavaApiServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcsJavaApiServicesApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
+}
---
+++ src/test/java/com/tcsl/javaapi/TcsJavaApiServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcsl.javaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcsJavaApiServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
